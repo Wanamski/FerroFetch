18b36d6e14fbe71adfce0aee58a02c5e4e859f2b
Wanamski <wnmsk@mailbox.org>
2024-10-27 18:55:10 +0100

    added basic functionality for reading toml file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use serde_derive::Deserialize;
+use std::{fs::File, io::Read, process::exit};
+use toml;
+
+#[derive(Deserialize)]
+struct Conf {
+    print_info: PrintInfo,
+}
+
+#[derive(Deserialize)]
+struct PrintInfo {
+    os: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // reading contents from config file into String
+    let conf_path = "./ff_conf.toml";
+    let mut conf_file = File::open(conf_path).expect("failed to open the config file...");
+    let mut conf_contents = String::new();
+    conf_file
+        .read_to_string(&mut conf_contents)
+        .expect("failed to read config file...");
+
+    let conf_data: Conf = match toml::from_str(&conf_contents) {
+        Ok(d) => d,
+        Err(_) => {
+            eprintln!("Could not load config file from {conf_path}");
+            exit(1);
+        }
+    };
+
+    println!("OS in config set: {}", conf_data.print_info.os);
 }
